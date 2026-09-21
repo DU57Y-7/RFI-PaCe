0e6886a1eb651ef1a3a58dc04ba104172f549089
DU57Y-7 <dustinjm07@gmail.com>
2026-09-21 11:16:36 +1000

    Update team.js

--- assets/js/team.js
+++ assets/js/team.js
@@ -69,7 +69,7 @@ const TEAM = [
   },
   {
     name: "Max Gadsby",
-    role: "Microwave Engineering",
+    role: "Microwave, Analog & Digital Engineering",
     university: "Griffith University — PhD Candidate",
     linkedin: "https://www.linkedin.com/in/max-gadsby-4148b926a/",
     photo: "assets/img/team/max.png",
